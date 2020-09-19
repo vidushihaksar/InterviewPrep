5c6af7db62473baea6e7cffc356620fc11ceb353
Vidushi Haksar <60004269+vidushihaksar@users.noreply.github.com>
2020-09-19 20:36:55 +0530

    first commit

---
+++ GFG_Sort_012.java
@@ -0,0 +1,50 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int T = scn.nextInt();
+		
+		for(int t = 0; t <T; t++){
+		    int n = scn.nextInt();
+		    int[]a = new int[n];
+		    for(int e=0; e < n; e++){
+		        a[e] = scn.nextInt();
+		    }
+		    
+		    helper(a);
+		    
+		    StringBuilder sb = new StringBuilder();
+		    for(int ele : a){
+		        sb.append(ele+" ");
+		    }
+		    System.out.println(sb);
+		}
+	}
+	
+	public static void helper(int []a){
+	    int i = 0, j=0, k = a.length-1;
+	    
+	    while(j <= k){
+	        if(a[j] == 1) { j++; }
+	        else if(a[j] == 0){
+	            int temp = a[i];
+	            a[i] = a[j];
+	            a[j] = temp;
+	            i++;
+	            j++;
+	        }
+	        else{
+	            int temp = a[j];
+	            a[j] = a[k];
+	            a[k] = temp;
+	            k--;
+	        }
+	        
+	    }
+	}
+}
